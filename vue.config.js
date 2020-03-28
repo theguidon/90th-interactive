5aacda3fbcb495fdd1f0a27d49aa4a95332f2ffb
lrnzpn <migzpinaroc@gmail.com>
2020-03-28 22:27:35 +0800

    oops forgot smth

--- vue.config.js
+++ vue.config.js
@@ -1,23 +1,23 @@
-// const ImageminPlugin = require("imagemin-webpack-plugin").default;
+const ImageminPlugin = require("imagemin-webpack-plugin").default;
 
 module.exports = {
-  //   chainWebpack: config => {
-  //     config.module
-  //       .rule("images")
-  //       .use("url-loader")
-  //       .loader("url-loader")
-  //       .tap(options => Object.assign(options, { limit: 8000 }));
-  //   },
-  //   configureWebpack: {
-  //     devtool: "source-map",
-  //     plugins: [
-  //       new ImageminPlugin({
-  //         pngquant: {
-  //           quality: "90-95"
-  //         }
-  //       })
-  //     ]
-  //   },
+  chainWebpack: config => {
+    config.module
+      .rule("images")
+      .use("url-loader")
+      .loader("url-loader")
+      .tap(options => Object.assign(options, { limit: 8000 }));
+  },
+  configureWebpack: {
+    devtool: "source-map",
+    plugins: [
+      new ImageminPlugin({
+        pngquant: {
+          quality: "90-95"
+        }
+      })
+    ]
+  },
   css: {
     loaderOptions: {
       sass: {
